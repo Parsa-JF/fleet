1eee0dd497514b042584c176e87df1223f7c7dca
parsa-jf <parsafares@gmail.com>
2023-06-01 18:09:45 -0700

    patch harness

--- fuzz/fuzz_targets/install_linker.rs
+++ fuzz/fuzz_targets/install_linker.rs
@@ -3,5 +3,5 @@ use libfuzzer_sys::fuzz_target;
 use fleet_rs::utils::configure::install_linker;
 
 fuzz_target!(|linker: String| {
-    install_linker(&linker);
+    let _ = install_linker(&linker);
 });
